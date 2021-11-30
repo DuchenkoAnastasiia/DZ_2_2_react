f66ddfd114fa2fe329d5724393ad773bbf8968ec
Anastasiia Duchenko <nduchenko2001@gmail.com>
2021-11-30 18:01:36 +0200

    first commit

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,48 @@
+import styles from "./Music.module.css";
+
+const Music = () => {
+    return (
+        <>
+            <h2 className={styles.title}>Miyagi & Andy Panda</h2>
+            <img className={styles.img_1} src="https://lastfm.freetls.fastly.net/i/u/ar0/859026e4aa921ab4611bb3941a1c2123.jpg" alt="-"/>
+            <h3 className={styles.title}>Инфо об участниках:</h3>
+            <h3 className={styles.title}>MiyaGi</h3>
+            <img className={styles.img_2} src="https://uznayvse.ru/person/miyagi/MiyaGi01.jpg" alt="-"/>
+            <p className={styles.info}>Кудзаев Азамат Казбекович родился 13 декабря 1990 года во Владикавказе. Отец — хирург и руководитель «Центра ортопедии и эстетической хирургии» в Северной Осетии. Азамат окончил медицинскую академию. Работал хирургом, но в скором времени бросил и стал заниматься музыкой. Занимается рэпом с 2000 года. В 2020—2021 гг. выступал под псевдонимом Shau, затем сменил его на Miyagi. Свой псевдоним взял в честь мистера Мияги из фильма «Парень-каратист». В 2009 году Азамат вступил в творческое объединение Dope Records, куда также, помимо Эндшпиля, входили Намо Миниган, Shuga, Асаб, SH Kera, группа The GuyS. В августе 2014 года Азамат основал собственный лейбл Asata, куда, помимо него, вошли Намо Миниган и Amigo. За время пребывания в Asata Азамат выпустил, ставшие успешными на тот момент, клипы на треки «Колибри», «Дом», «Бонни». В сентябре 2017 года выпустил сольный сингл «Sunshine».18 мая 2018 года, после долгого перерыва был выпущен сингл «Сонная Лощина». За 2018 год было выпущено ещё 3 сингла: «Captain», «Sorry», «Родная пой» (при уч. KADI).
+            Азамат стал гостем в треке KADI «Colors».За 2019 год было выпущено несколько синглов: «Fantasy» «Angel», «Bismarck» (при уч. TumaniYO и KADI), «Trenchtown». 21 июня 2019 года вышел дебютный альбом Miyagi «Buster Keaton», включивший в себя 12 треков, также в альбоме присутствуют совместные треки с участниками Hajime Records: Andy Panda (Эндшпиль), HLOY, TumaniYO и KADI. 13 сентября 2019 года был выпущен трек под названием «Jamm» (при уч. TumaniYO). 12 декабря 2019 года вышел анимационный клип, посвящённый Бобу Марли, на песню «Trenchtown». Вместе со Скриптонитом принял участие в треке 104 «НЕ ЖАЛЬ» из альбома «КИНО БЕЗ СИГАРЕТ».
+            2 марта 2020 года был выпущен клип на песню «Самурай». 9 марта был перевыпущен трек «Texture» с обновлённым инструменталом. 29 апреля вышел совместный трек с новым резидентом Hajime Records — Ollane «Touch The Sky».25 сентября вышел альбом KADI под названием «KOPFKINO» где MiyaGi поучаствовал в треке «Prayers».</p>
+            <h3 className={styles.title}>Andy Panda</h3>
+            <img className={styles.img_3} src="https://i1.sndcdn.com/artworks-000477031989-yi2esi-t500x500.jpg" alt="-"/>
+            <p className={styles.info}>Сослан Бурнацев родился 2 октября 1995 года во Владикавказе. В юности играл в футбол за юношескую команду «Спартак» (Владикавказ). Получил образование пищевого технолога. Начал заниматься рэпом в 16 лет. Свой первый получивший известность псевдоним «Эндшпиль» взял под впечатлением от фильма «Адский эндшпиль». В 2012 году вступил в объединение Dope Records, которое покинул в 2014 году. В том же году выпустил дебютный сольный альбом «Накипь», а через год выпустил второй альбом «Тютелька в тютельку». Также в 2015 году вышел совместный альбом Эндшпиля и владикавказского рэпера SH Kera «Бездельники».
+            В марте 2018 года, после полугодового перерыва был выпущен сингл «It’s My Life» c участием TumaniYO. Сослан представился с новым псевдонимом Andy Panda, выпустив трек «Intro». За 2018 год было выпущено всего лишь 2 сингла: «Коконъ» и «Брат передал». На второй сингл Сослан выпустил клип на YouTube.
+            8 марта следующего года исполнитель Dose, работавший на тот момент со Скриптонитом, решил порадовать поклонников совместным треком с Andy Panda под названием «Танцевала». За 2019 год было выпущено несколько синглов: «I Wanna Feel The Love», совместно с Castle; «Billboard», совместно со Скриптонитом, 104, TumaniYO и MiyaGi; «Sun», совместно с TumaniYO и Niman; «Замёрз», на который также был выпущен клип, совместно со Скриптонитом. Принял участие в треке «Говори мне», который входит в дебютный сольный альбом MiyaGi.
+            В 18 октября 2019 года был выпущен первый сингл из EP, совместно с TumaniYO и MiyaGi, под названием «Jumanji». В ноябре вышел новый EP «King Kong», в который вошли 8 треков. В альбоме также приняли участие Pastor Napas из группы «ОУ74», Змей из «Касты», MiyaGi, TumaniYO. Принял непосредственное участие в одном из треков с совместного EP Хаски и масло чёрного тмина «У». Через некоторое время поcле релиза EP «King Kong» был выпущен клип на «Rude Mantras». Сослан принял участие в альбоме Скриптонита «2004» в треке «Привычка».
+            Сослан принял участие в совместном альбоме 104 и Saluki "СТЫД ИЛИ СЛАВА" в треке "АНГЕЛ". Этот альбом был выпущен 30 апреля 2021 года.</p>
+            <h3 className={styles.title}>Альбомы:</h3>
+            <div className={styles.albom}>
+                <div>
+                    <h4 className={styles.title}>Hajime, Pt. 1</h4>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2SEiZFfUrbXphtiN7SsbZRhMPzxb0AiYa38ItkvY&usqp=CAE&s" alt="-"/>
+                </div>
+                <div>
+                    <h4 className={styles.title}>Hajime, Pt. 2</h4>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAdscPITFsEESJczbFZvM_7p8OPrQcdor0Pt0p9xY&usqp=CAE&s" alt="-"/>
+                </div>
+                <div>
+                    <h4 className={styles.title}>Умшакалака</h4>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMrw72KhEn1nx6dwAd4TyGC4Hw5x7qLIkERMxwF2I&usqp=CAE&s" alt="-"/>
+                </div>
+                <div>
+                    <h4 className={styles.title}>Hajime, pt. 3</h4>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQg-bBwuyWhMAHhfzI3nzlgTSfYJoEh8jsz7Ojsd4&usqp=CAE&s" alt="-"/> 
+                </div>
+                <div>
+                    <h4 className={styles.title}>YAMAKASI</h4>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu1_JiVBL3kpW7IVMW48MWqAN0XA10yqBXqPzldE0&usqp=CAE&s" alt="-"/>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default Music;
